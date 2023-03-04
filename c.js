cb463d0ddf4efa1c7d89f8d95e8c5418ae35553c
ptlsx <446719004@qq.com>
2023-03-04 18:21:34 +0800

    Initial commit

---
+++ c.js
@@ -0,0 +1,4 @@
+﻿document.writeln("<div class='contact'>");
+document.writeln("<strong><a href='index.html'>推荐款</a> <a href='notice.html' style='white-space:nowrap;'>购物须知</a><br/>QQ:0000<br/>");
+document.writeln("V信:0000</strong><br/>");
+document.writeln("</div>");
---
+++ h.js
@@ -0,0 +1,7 @@
+﻿document.writeln("<!DOCTYPE html>");
+document.writeln("<html>");
+document.writeln("<head>");
+document.writeln("<meta name = 'format-detection' content='telephone = no' />");
+document.writeln("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />");
+document.writeln("<link rel='stylesheet' href='css.css' type='text/css'/>");
+document.writeln("<script src='cnzz2254h.js'></script>");
